cffaa2806c0a52061061c24c3f912c0ec64d0cfb
Mikkel <MikkelHansen95@live.dk>
2020-05-27 22:53:06 +0200

    mongo update

--- Mongo4Exam/index.js
+++ Mongo4Exam/index.js
@@ -9,6 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
+var version = "v1";
 //INIT MONGODB IF not existing
 //Populate DB
 try{
@@ -18,62 +19,42 @@ try{
 }
 
 
-app.get('/courses', function (req,res) {
-    //console.log(req)
+app.get('/api/' + version + '/courses', function (req,res) {
+    console.log('/courses')
     res.setHeader('Content-Type', 'application/json');
     return mongo.findCoursesWithParams(req,res);
-    /*
-    if(req.query.hasOwnProperty("price")){
-        return mongo.findCoursesWithPrice(req,res);
-    }else if(req.query.hasOwnProperty("tag")){
-        return mongo.findCoursesWithTag(req,res);
-    }else if(req.query.hasOwnProperty("level")){
-        return mongo.findCoursesWithLevel(req,res);
-    }else{
-        return mongo.findAll(req,res);
-    }
-    */
 });
 
-app.get('/courses/tags/top10', function (req,res) {
+app.get('/api/' + version + '/courses/tags/top10', function (req,res) {
     console.log('/courses/tags')
     res.setHeader('Content-Type', 'application/json');
     return mongo.findCountOfTags(req,res);
 });
 
 // NEEDS TO BE LAST OTHERWISE THE CALLS ALWAYS POINT AT THIS
-app.get('/courses/:id', function (req,res) {
+app.get('/api/' + version + '/courses/:id', function (req,res) {
     console.log('/courses/:id')
     res.setHeader('Content-Type', 'application/json');
     return mongo.findById(req,res);
 });
 
-app.delete('/courses/:id',function (req,res) {
+app.delete('/api/' + version + '/courses/:id',function (req,res) {
     console.log('/courses/:id')
     res.setHeader('Content-Type', 'application/json');
     return mongo.deleteById(req,res);
 });
 
-app.put('/courses/:id', function (req,res) {
+app.put('/api/' + version + '/courses/:id', function (req,res) {
     console.log('PUT /courses/:id')
     res.setHeader('Content-Type', 'application/json');
     return mongo.updateById(req,res);
 });
 
-app.post('/courses/', function (req,res) {
+app.post('/api/' + version + '/courses/', function (req,res) {
     console.log('POST /courses/')
     res.setHeader('Content-Type', 'application/json');
     return mongo.addDocument(req,res);
 });
-
-function createLogJsonReq(req){
-    let request = {
-        headers: req.headers,
-        url: req.originalUrl,
-        body: req.body
-    }
-    return JSON.stringify(request);
-}
 
 
 const server = app.listen(port, () => console.log(`MongoDB API ready and listening at http://localhost:${port}`))
